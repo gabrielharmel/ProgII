03643385cd4f3400f62ba46c4f6610925a76a58f
Gabriel Harmel <gabrielh.gta@gmail.com>
2020-11-16 11:21:43 -0300

    Alterações para a AV5

--- myproject/backend/modelo.py
+++ myproject/backend/modelo.py
@@ -1,21 +1,74 @@
 from config import *
 
+
+class Montadora(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(254))
+    pais_origem = db.Column(db.String(254))
+
+    def __str__(self):
+        return str(self.id)+", " + self.nome + ", " +\
+            self.pais_origem
+
+    def json(self):
+        return ({
+            "id": self.id,
+            "nome": self.nome,
+            "pais_origem": self.pais_origem,
+        })
+
+
 class Carro(db.Model):
     # atributos do carro
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(254))
     cor = db.Column(db.String(254))
     ano = db.Column(db.String(254))
+    montadora_id = db.Column(db.Integer, db.ForeignKey(Montadora.id))
+    montadora = db.relationship("Montadora")
 
     # método para expressar o carro em forma de texto
     def __str__(self):
-        return str(self.id)+") "+ self.nome + ", " +\
-            self.cor + ", " + self.ano
+        c = f"Carro {self.nome}"
+        if self.montadora_id != None:
+            c += f"produzido pela montadora {self.nome} localizada em {self.pais_origem}"
+            return c
+    
     # expressao no formato json 
-    def json(self): 
-        return { 
-            "id": self.id, 
-            "nome": self.nome, 
-            "cor": self.cor, 
-            "ano": self.ano 
-        }
+    def json(self):
+        if self.montadora_id is None:
+            return ({
+                "id": self.id,
+                "nome": self.nome,
+                "cor": self.cor,
+                "ano": self.ano
+            })
+        else:
+            return ({
+                "id": self.id,
+                "nome": self.nome,
+                "cor": self.cor,
+                "ano": self.ano,
+                "montadora": self.montadora.json()
+            })
+
+class Cliente(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(254))
+    estado_civil = db.Column(db.String(254))
+    idade = db.Column(db.String(254))
+    carro_id = db.Column(db.Integer, db.ForeignKey(Carro.id), nullable=False)
+    carro = db.relationship("Carro")
+
+    def __str__(self):
+        return str(self.id)+", " + self.nome + ", " +\
+            self.estado_civil+", " + self.idade
+
+    def json(self):
+        return ({
+            "id": self.id,
+            "nome": self.nome,
+            "estado_civil": self.estado_civil,
+            "idade": self.idade,
+            "carro": self.carro.json()
+        })
--- myproject/backend/test.py
+++ myproject/backend/test.py
@@ -7,23 +7,51 @@ if __name__ == "__main__":
         os.remove(arquivobd)
 
     db.create_all()
+    montadora1 = Montadora(nome = "Ford", pais_origem = "Estados Unidos")
+    montadora2 = Montadora(nome = "Nissan", pais_origem =  "Japão")
+    montadora3 = Montadora(nome = "Volkswagen", pais_origem = "Alemanha")
+    montadora4 = Montadora(nome = "Dodge", pais_origem ="Estados Unidos")
+    montadora5 = Montadora(nome = "Chevrolet", pais_origem = "Estados Unidos")
+    montadora6 = Montadora(nome = "Plymouth", pais_origem = "Estados Unidos")
+    
+    carro1 = Carro(nome = "Focus", cor = "Prata", 
+        ano = "2004", montadora_id = 1)
+    carro2 = Carro(nome = "Pampa", cor = "Verde", 
+        ano = "1997", montadora_id = 1)
+    carro3 = Carro(nome = "Versa", cor = "Marrom", 
+        ano = "2016", montadora_id = 2)
+    carro4 = Carro(nome = "Gol", cor = "Branco", 
+        ano = "2017", montadora_id = 3)
+    carro5 = Carro(nome = "Charger", cor = "Laranja", 
+        ano = "1975", montadora_id = 4)
+    carro6 = Carro(nome = "Chevelle", cor = "Vermelho", 
+        ano = "1970", montadora_id = 5)
+    carro7 = Carro(nome = "Roadrunner", cor = "Prata", 
+        ano = "1970", montadora_id = 6)
 
-    # teste da classe Carro
-    carro1 = Carro(nome = "Ford Focus", cor = "Prata", 
-        ano = "2004")
-    carro2 = Carro(nome = "Ford Pampa", cor = "Verde", 
-        ano = "1997")
-    carro3 = Carro(nome = "Nissan Versa", cor = "Marrom", 
-        ano = "2016")
-    carro4 = Carro(nome = "Volkswagen Gol", cor = "Branco", 
-        ano = "2017")
-    carro5 = Carro(nome = "Dodge Charger", cor = "Laranja", 
-        ano = "1975")
-    carro6 = Carro(nome = "Chevrolet Chevelle", cor = "Vermelho", 
-        ano = "1970")
-    carro7 = Carro(nome = "Plymouth Roadrunner", cor = "Prata", 
-        ano = "1970")
+    cliente1 = Cliente(nome = "Claudionei", estado_civil = "Casado",
+    idade = "43", carro_id = "7")
+    cliente2 = Cliente(nome = "Babilônio",  estado_civil = "Casado",
+    idade = "67", carro_id = "1")
+    cliente3 = Cliente(nome = "Brad",  estado_civil = "Solteiro",
+    idade = "18", carro_id = "6")
+    cliente4 = Cliente(nome = "Lara",  estado_civil = "Solteira",
+    idade = "21", carro_id = "2")
+    cliente5 = Cliente(nome = "Francielle",  estado_civil = "Solteira",
+    idade = "30", carro_id = "5")
+    cliente6 = Cliente(nome = "Luana",  estado_civil = "Casada",
+    idade = "27", carro_id = "3")
+    cliente7 = Cliente(nome = "Michael",  estado_civil = "Casado",
+    idade = "73", carro_id = "4")
+
     # persistir
+    db.session.add(montadora1)
+    db.session.add(montadora2)
+    db.session.add(montadora3)
+    db.session.add(montadora4)
+    db.session.add(montadora5)
+    db.session.add(montadora6)
+    
     db.session.add(carro1)
     db.session.add(carro2)
     db.session.add(carro3)
@@ -31,7 +59,23 @@ if __name__ == "__main__":
     db.session.add(carro5)
     db.session.add(carro6)
     db.session.add(carro7)
+
+    db.session.add(cliente1)
+    db.session.add(cliente2)
+    db.session.add(cliente3)
+    db.session.add(cliente4)
+    db.session.add(cliente5)
+    db.session.add(cliente6)
+    db.session.add(cliente7)
+
     db.session.commit()
+
+    print(montadora1.json()) 
+    print(montadora2.json())
+    print(montadora3.json())
+    print(montadora4.json())
+    print(montadora5.json())
+    print(montadora6.json())
 
     print(carro1.json())
     print(carro2.json())
@@ -40,3 +84,12 @@ if __name__ == "__main__":
     print(carro5.json())
     print(carro6.json())
     print(carro7.json())
+
+    print(cliente1.json())
+    print(cliente2.json())
+    print(cliente3.json())
+    print(cliente4.json())
+    print(cliente5.json())
+    print(cliente6.json())
+    print(cliente7.json())
+    
