9f3b3d80f75a1abe86abf576bbbb5b5b6d1443c2
Gabriel Harmel <gabrielh.gta@gmail.com>
2020-12-08 20:08:58 -0300

    finalização da AV6

--- myproject/backend/servidor.py
+++ myproject/backend/servidor.py
@@ -1,21 +1,22 @@
 from config import *
-from modelo import Carro
+from modelo import Montadora, Carro, Cliente
 
 @app.route("/")
 def inicio():
     return 'Sistema de cadastro de carros. '+\
-        '<a href="/listar_carros">Listar Carros</a>'
+        '<a href="/listar/Carro">Listar Carros</a>'
 #ROTA PARA LISTAR CARROS
-@app.route("/listar_carros")
-def listar_carros():
-    # obter os carros do cadastro
-    carros = db.session.query(Carro).all()
-    # método json para cada elemento da lista
-    carros_json = [ x.json() for x in carros ]
-    # fornecer a lista de carros para a página que exibe os carros
-    return jsonify(carros_json)
-    # permitir resposta para outras pedidos oriundos de outras tecnologias
-    resposta = jsonify(carros_json)
+@app.route("/listar/<string:classe>")
+def listar(classe):
+    dados = None
+    if classe == "Montadora":
+        dados = db.session.query(Montadora).all()
+    elif classe == "Carro":
+        dados = db.session.query(Carro).all()
+    elif classe == "Cliente":
+        dados = db.session.query(Cliente).all()
+    lista_jsons = [x.json() for x in dados]
+    resposta = jsonify(lista_jsons)
     resposta.headers.add("Access-Control-Allow-Origin", "*")
     return resposta 
 
--- myproject/frontend/js/menuscript.js
+++ myproject/frontend/js/menuscript.js
@@ -1,8 +1,39 @@
-$(function() { 
+$(function() {
+
+    function exibir_montadoras() {
+        $.ajax({
+            url: 'http://localhost:5000/listar/Montadora',
+            method: 'GET',
+            dataType: 'json',
+            success: listar,
+            error: function() {
+                alert("erro ao ler dados, verifique o backend");
+            }
+        });    
+        function listar (resposta) {
+            $('#corpoTabelaMontadoras').empty();
+            mostrar_conteudo('ListarMontadoras');
+            for (var i in resposta) {
+                lin = '<tr id="linha_'+resposta[i].id+'">' + 
+                '<td>' + resposta[i].nome + '</td>' + 
+                '<td>' + resposta[i].pais_origem + '</td>' +  
+                '<td><a href=# id="excluir_' + resposta[i].id + '" ' + 
+                  'class="excluir_montadora"><img src="imagens/delete.png" '+
+                  'alt="Excluir montadora" title="Excluir montadora"></a>' + 
+                '</td>' + 
+                '</tr>';
+                $('#corpoTabelaMontadoras').append(lin);
+            }
+        }
+    }
+    
+    $(document).on("click", "#linkListarMontadoras", function() {
+        exibir_montadoras();
+    });
     
     function exibir_carros() {
         $.ajax({
-            url: 'http://localhost:5000/listar_carros',
+            url: 'http://localhost:5000/listar/Carro',
             method: 'GET',
             dataType: 'json',
             success: listar,
@@ -13,83 +44,85 @@ $(function() {
 
         function listar (resposta) {
             $('#corpoTabelaCarros').empty();
-            mostrar_conteudo('TabelaCarros');
+            mostrar_conteudo('ListarCarros');
             for (var i in resposta) {
-                lin = '<tr>' +
+                lin = '<tr id="linha_'+resposta[i].id+'">' + 
                 '<td>' + resposta[i].nome + '</td>' + 
                 '<td>' + resposta[i].cor + '</td>' + 
                 '<td>' + resposta[i].ano + '</td>' + 
+                '<td>' + resposta[i].montadora.nome + '</td>' + 
                 '<td><a href=# id="excluir_' + resposta[i].id + '" ' + 
                   'class="excluir_carro"><img src="imagens/delete.png" '+
                   'alt="Excluir carro" title="Excluir carro"></a>' + 
                 '</td>' + 
                 '</tr>';
-                
-                ;
                 $('#corpoTabelaCarros').append(lin);
             }
         }
     }
 
-function mostrar_conteudo(identificador) {
-    $("#TabelaCarros").addClass('invisible');
-    $("#conteudoInicial").addClass('invisible');
-    $("#"+identificador).removeClass('invisible');      
-}
-
-$(document).on("click", "#linkListarCarros", function() {
-    exibir_carros();
-});
-
-$(document).on("click", "#linkInicio", function() {
-    mostrar_conteudo("conteudoInicial");
-});
-
-$(document).on("click", "#btnIncluirCarro", function validarform() {
-    if ((document.getElementById("campoNome").value.length < 1) || (document.getElementById("campoCor").value.length < 1) || 
-    (document.getElementById("campoAno").value.length < 1)) {
-        alert('Por favor, preencha todos os campos');
-    } 
-    else {
-        nome = $("#campoNome").val();
-        cor = $("#campoCor").val();
-        ano = $("#campoAno").val();
-        var dados = JSON.stringify({ nome: nome, cor: cor, ano: ano});
-        $.ajax({
-            url: 'http://localhost:5000/incluir_carro',
-            type: 'POST',
-            dataType: 'json',
-            contentType: 'application/json',
-            data: dados,
-            success: carroIncluido,
-            error: erroAoIncluir
-        });
+    function mostrar_conteudo(identificador) {
+        $("#ListarCarros").addClass('d-none');
+        $("#conteudoInicial").addClass('d-none');
+        $("#ListarClientes").addClass('d-none');
+        $("#ListarMontadoras").addClass('d-none')
+        $("#"+identificador).removeClass('d-none');      
     }
-    function carroIncluido (retorno) {
-        if (retorno.resultado == "ok") {
-            alert("Carro incluído com sucesso!");
-            $("#campoNome").val("");
-            $("#campoCor").val("");
-            $("#campoAno").val("");
+
+    $(document).on("click", "#linkListarCarros", function() {
+        exibir_carros();
+    });
+
+    $(document).on("click", "#linkInicio", function() {
+        mostrar_conteudo("conteudoInicial");
+    });
+
+    $(document).on("click", "#btnIncluirCarro", function validarform() {
+        if ((document.getElementById("campoNome").value.length < 1) || (document.getElementById("campoCor").value.length < 1) || 
+        (document.getElementById("campoAno").value.length < 1)) 
+        {
+            alert('Por favor, preencha todos os campos');
         } 
         else {
-            alert(retorno.resultado + ":" + retorno.detalhes);
-        }            
-    }
-    function erroAoIncluir (retorno) {
-        alert("ERRO: "+retorno.resultado + ":" + retorno.detalhes);
-    }
-});
+            nome = $("#campoNome").val();
+            cor = $("#campoCor").val();
+            ano = $("#campoAno").val();
+            var dados = JSON.stringify({ nome: nome, cor: cor, ano: ano});
+            $.ajax({
+                url: 'http://localhost:5000/incluir_carro',
+                type: 'POST',
+                dataType: 'json',
+                contentType: 'application/json',
+                data: dados,
+                success: carroIncluido,
+                error: erroAoIncluir
+            });
+        }
+        function carroIncluido (retorno) {
+            if (retorno.resultado == "ok") {
+                alert("Carro incluído com sucesso!");
+                $("#campoNome").val("");
+                $("#campoCor").val("");
+                $("#campoAno").val("");
+            } 
+            else {
+                alert(retorno.resultado + ":" + retorno.detalhes);
+            }            
+        }
+        function erroAoIncluir (retorno) {
+            alert("ERRO: "+retorno.resultado + ":" + retorno.detalhes);
+        }
+    });
 
-$('#modalIncluirCarro').on('hide.bs.modal', function (e) {
-    if (! $("#TabelaCarros").hasClass('invisible')) {
-        exibir_carros();
-    }
-});
+    $('#modalIncluirCarro').on('hide.bs.modal', function (e) {
+        if (! $("#ListarCarros").hasClass('d-none')) {
+            exibir_carros();
+        }
+    });
 
-mostrar_conteudo("conteudoInicial");
+    mostrar_conteudo("conteudoInicial");
 
-$(document).on("click", ".excluir_carro", function() { 
+    $(document).on("click", ".excluir_carro", function() { 
         var componente_clicado = $(this).attr('id'); 
         var nome_icone = "excluir_"; 
         var id_carro = componente_clicado.substring(nome_icone.length); 
@@ -113,8 +146,38 @@ $(document).on("click", ".excluir_carro", function() {
             alert("Erro ao excluir dados, verifique o backend: ");
         }
     });
+
+    function exibir_clientes() {
+        $.ajax({
+            url: 'http://localhost:5000/listar/Cliente',
+            method: 'GET',
+            dataType: 'json',
+            success: listar,
+            error: function() {
+                alert("erro ao ler dados, verifique o backend");
+            }
+        });    
+        function listar (resposta) {
+            $('#corpoTabelaClientes').empty();
+            mostrar_conteudo('ListarClientes');
+            for (var i in resposta) {
+                lin = '<tr id="linha_'+resposta[i].id+'">' + 
+                '<td>' + resposta[i].nome + '</td>' + 
+                '<td>' + resposta[i].estado_civil + '</td>' + 
+                '<td>' + resposta[i].idade + '</td>' + 
+                '<td>' + resposta[i].carro.nome + '</td>' + 
+                '<td><a href=# id="excluir_' + resposta[i].id + '" ' + 
+                  'class="excluir_cliente"><img src="imagens/delete.png" '+
+                  'alt="Excluir cliente" title="Excluir cliente"></a>' + 
+                '</td>' + 
+                '</tr>';
+                $('#corpoTabelaClientes').append(lin);
+            }
+        }
+    }
+    
+    $(document).on("click", "#linkListarClientes", function() {
+        exibir_clientes();
+    });
+
 });
-
-
-
-
